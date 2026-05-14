37280e975b37b92322570438d617dc961f5e550b
David Ta <david.ta@saylor.org>
2026-05-14 16:15:44 -0500

    Privacy provider: sort interfaces + strip blank line after class brace

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -46,9 +46,8 @@ use core_privacy\local\request\writer;
  */
 class provider implements
     \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\plugin\provider,
-    \core_privacy\local\request\core_userlist_provider {
-
+    \core_privacy\local\request\core_userlist_provider,
+    \core_privacy\local\request\plugin\provider {
     /**
      * Describe what user data this plugin stores.
      *
